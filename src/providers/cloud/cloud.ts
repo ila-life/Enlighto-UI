8e426e9c10768bf1bdfeb6d1024d8cd43a3d2f5f
Sooraj Parameswaran <srjpn@gmail.com>
2020-03-30 14:46:47 +0530

    Added two more songs

--- src/providers/cloud/cloud.ts
+++ src/providers/cloud/cloud.ts
@@ -3,7 +3,7 @@ import { of } from 'rxjs';
 
 @Injectable()
 export class CloudProvider {
-  files:any = [
+  files: any = [
     {
       url: 'http://drive.google.com/uc?export=view&id=10xdzKy6qTePm4IkUU8VkaJRDVB7VVXcA',
       name: 'Perfect by Ed Sheeran'
@@ -12,8 +12,13 @@ export class CloudProvider {
       url: 'https://ia801609.us.archive.org/16/items/nusratcollection_20170414_0953/Man%20Atkiya%20Beparwah%20De%20Naal%20Nusrat%20Fateh%20Ali%20Khan.mp3',
       name: 'Man Atkeya Beparwah by Nusrat Fateh Ali Khan'
     },
-    { url: 'https://ia801503.us.archive.org/15/items/TheBeatlesPennyLane_201805/The%20Beatles%20-%20Penny%20Lane.mp3',
-      name: 'Penny Lane by The Beatles'
+    {
+      url: 'http://drive.google.com/uc?export=view&id=1gE7v-0P4JC7xR95pjuxXzKcErgnE_ttb',
+      name: 'Confusing Facts'
+    },
+    {
+      url: 'http://drive.google.com/uc?export=view&id=1nZinEKhU5TIOiAG2b2BjpUg39fmLPFEL',
+      name: 'Jijonte paratta voice'
     }
   ];
   getFiles() {
