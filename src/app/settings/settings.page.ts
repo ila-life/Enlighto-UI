71dc0e532e9a42dca6f890e445ad8a4ee7bd4648
Sooraj Parameswaran <srjpn@gmail.com>
2020-04-18 12:33:56 +0530

    Removed unwanted imports

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { NavController } from '@ionic/angular';
-import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-settings',
@@ -10,35 +6,5 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./settings.page.scss']
 })
 export class SettingsPage {
-  user: any;
-  userImage: 'assets/img/user.jpg';
-  photo: SafeResourceUrl;
-  constructor(
-    private storage: Storage,
-    private navCtrl: NavController,
-    private sanitizer: DomSanitizer
-  ) {}
-
-  ionViewWillEnter() {
-    this.storage.get('currentUser').then(value => {
-      this.user = value;
-    });
-  }
-
-  goHome() {
-    this.navCtrl.navigateBack('/menu/tabs/home');
-  }
-
-  async takePicture() {
-    const image = await Plugins.Camera.getPhoto({
-      quality: 100,
-      allowEditing: false,
-      resultType: CameraResultType.DataUrl,
-      source: CameraSource.Camera
-    });
-
-    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
-      image && image.dataUrl
-    );
-  }
+  constructor() {}
 }
