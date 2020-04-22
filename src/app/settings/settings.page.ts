70d21bfe7a09ff2290f259ef64d4ffbb68b67847
Sooraj Parameswaran <srjpn@gmail.com>
2020-04-22 22:25:07 +0530

    Added social share link and fab is taken off

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-settings',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.page.scss']
 })
 export class SettingsPage {
-  constructor() {}
+  message = 'Now you can learn at any time.\n\nEnlighto is introducing to you audio classes. You can now learn in between any routine activities like traveling, working and pretty much anything for PSC exams and any other competitive exams for free absolutely. Listen and learn the classes\n\nDownload from play store\n\nhttps://play.google.com/store/apps/details?id=com.ilalife.enlighto\n\nFollow us on\n\nwww.facebook.com/enlightoonline\n\nhttps://instagram.com/enlightoonline'
+  constructor(private socialSharing: SocialSharing) {}
+
+  share() {
+    this.socialSharing.share(this.message, 'Enlighto');
+  }
 }
